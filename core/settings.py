796af3cf739818a1c923c4ef160b27ea83c3d8cb
TamunaChumbadze <t.chumbadze@gmail.com>
2026-03-10 22:21:15 +0400

    Add JWT authentication

--- core/settings.py
+++ core/settings.py
@@ -128,6 +128,11 @@ if not DEBUG:
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+    ),
+
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 5,
 }
